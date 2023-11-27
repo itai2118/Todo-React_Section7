ff752296e4d5fe05b07cc2cf0be09950db6afb4b
itai <147829442+itai2118@users.noreply.github.com>
2023-11-27 00:10:42 +0000

    Create 8 files

---
+++ src/Todo.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { InputTodo } from "./components/InputTodo.jsx";
+import { IncompleteTodos } from "./components/IncompleteTodos.jsx";
+import { CompleteTodos } from "./components/CompleteTodos.jsx";
+import "./styles.css";
+
+export const Todo = () => {
+  const [todoText, seTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    // "TODOです1",
+    // "TODOです2",
+  ]);
+  const [completeTodos, setCompleteTodos] = useState([
+    // "TODOでした1",
+    // "TODOでした2",
+  ]);
+
+  const onChangeTodoText = (event) => seTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    seTodoText("");
+  };
+
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+    const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  };
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
+  };
+  const isMaxLimitIncompelteTodos = incompleteTodos.length >= 5;
+
+  return (
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={isMaxLimitIncompelteTodos}
+      />
+      {isMaxLimitIncompelteTodos && (
+        <p style={{ color: "red" }}>登録できるTODOは5個までです。</p>
+      )}
+      <IncompleteTodos
+        todos={incompleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeTodos} onClickBack={onClickBack} />
+    </>
+  );
+};
